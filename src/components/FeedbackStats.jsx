b56f17f5ec1b378158dd9e6e94d80baac875548c
Willey Nascimento <willeynascimentodev@waan.com.br>
2022-03-31 13:34:03 -0300

    FeedbackStats

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,9 +1,23 @@
-function FeedbackStats ({feedback}) {
+import PropTypes from 'prop-types'
+
+function FeedbackStats ({ feedback }) {
+
+    //calculating the avarage
+    let average = feedback.reduce((acc, cur) => {
+        return acc + cur.rating
+    }, 0) / feedback.length
+
+    //removing any decimals zeros
+    average = average.toFixed(1).replace(/[.,]0$/, '')
+
     return <div className="feedback-stats">
         <h4>{feedback.length} Reviews </h4>
-        <h4> Avarage Rating 10 </h4>
-        
+        <h4>Average Rating { isNaN(average) ? 0 : average } </h4>
     </div>
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
 }
 
 
