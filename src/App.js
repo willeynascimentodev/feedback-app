5bc1390fc7ae64496f2e6d0766ccbd7f602c53f8
Willey Nascimento <willeynascimentodev@waan.com.br>
2022-03-31 12:50:27 -0300

    Adding numbers of reviews

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import Header from "./components/Header"
 import FeedbackList from './components/FeedbackList'
 import { useState } from 'react'
 import PropTypes from 'prop-types'
+import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/FeedbackData"
 
 function App () {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item ) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header text="Feedback UI" />
             <div className="container" >
-                <FeedbackList feedback={feedback} />
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={ deleteFeedback }/>
             </div>
         </>
     )
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,10 @@
+function FeedbackStats ({feedback}) {
+    return <div className="feedback-stats">
+        <h4>{feedback.length} Reviews </h4>
+        <h4> Avarage Rating 10 </h4>
+        
+    </div>
+}
+
+
+export default FeedbackStats
